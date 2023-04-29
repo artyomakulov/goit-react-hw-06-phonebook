904b624ec9d109e4c86725e55c1b8301fc0cedb1
artyomakulov <artyomakulov@gmail.com>
2023-04-29 12:49:59 +0300

    fix contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
 import Form from './components/Form/Form';
 import ContactList from './components/Contacts/Contacts';
 import Filter from './components/Filter/Filter';
-
 import css from './App.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteContact } from './redux/contacts/slice';
 
 function App() {
-  const contacts = useSelector(state => state.contacts);
-  const filter = useSelector(state => state.filter);
-  const dispatch = useDispatch();
-
-  const handleDeleteContact = contactId => {
-    dispatch(deleteContact(contactId));
-  };
-
-  const getVisibleContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
   return (
     <div className={css.section}>
       <h1>Phonebook</h1>
       <Form />
       <h2>Contacts</h2>
       <Filter />
-      <ContactList
-        contacts={getVisibleContacts()}
-        onDeleteContact={handleDeleteContact}
-      />
+      <ContactList />
     </div>
   );
 }
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import css from './Contacts.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact } from '../../redux/contacts/slice';
 
-export default function ContactList({ contacts, onDeleteContact }) {
+export default function ContactList() {
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
+
+  const handleDeleteContact = contactId => {
+    dispatch(deleteContact(contactId));
+  };
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const visibleContacts = getVisibleContacts();
   return (
     <ol className={css.ContactList}>
-      {contacts.map(({ id, name, number }) => {
+      {visibleContacts.map(({ id, name, number }) => {
         return (
           <li className={css.ContactList_item} key={id}>
             <p>
@@ -13,7 +30,7 @@ export default function ContactList({ contacts, onDeleteContact }) {
             </p>
             <button
               className={css.ContactList_btn}
-              onClick={() => onDeleteContact(id)}
+              onClick={() => handleDeleteContact(id)}
               type="button"
             >
               Delete
@@ -24,14 +41,3 @@ export default function ContactList({ contacts, onDeleteContact }) {
     </ol>
   );
 }
-
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
-  onDeleteContact: PropTypes.func.isRequired,
-};
